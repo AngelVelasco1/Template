f39e698210a3e36794f5fcef75e9bfa553649f70
Angel Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-05-01 15:29:44 -0500

    Add files via upload

--- components/api.js
+++ components/api.js
@@ -1,52 +1,54 @@
 export default {
-    allAPI() {
-
-    }
-}
-try {
-    /* DOM selections */
-    const renderNews = document.querySelector('#render-container');
-
-    /* Variables */
-    const country = 'us';
-
-    /* API settings */
-    const apiKey = `084e0cc135174cb4bad811a6571ae62a`
-    let urlNews = await fetch(`https://newsapi.org/v2/top-headlines?pageSize=100&country=${country}&apiKey=${apiKey}`);
-    const responseJson = await urlNews.json();
-
-    /* News */
-    const getNews = async () => {
-        const template = responseJson.articles.map((eachNew) => {
-            return `
+    async allAPI() {
+        try {
+            /* DOM selections */
+           
+            /* Variables */
+            const country = 'us';
         
-            <div class="news-card">
-                <div class="container-img">
-                    <img class="img_news" src="${eachNew.urlToImage || " ./img/newspaper.png"}" />
-                </div>
-                <div class="news-content">
-                    <div class="title_news">
-                        ${eachNew.title}
+            /* API settings */
+            const apiKey = `084e0cc135174cb4bad811a6571ae62a`
+            let urlNews = await fetch(`https://newsapi.org/v2/top-headlines?pageSize=100&country=${country}&apiKey=${apiKey}`);
+            const responseJson = await urlNews.json();
+        
+            /* templates */
+            const getNews = async () => {
+                const template = responseJson.articles.map((eachNew) => {
+                    return `
+                
+                    <div class="news-card">
+                        <div class="container-img">
+                            <img class="img_news" src="${eachNew.urlToImage || " ./img/newspaper.png"}" />
+                        </div>
+                        <div class="news-content">
+                            <div class="title_news">
+                                ${eachNew.title}
+                            </div>
+                            <div class="description-news">
+                                <p>${eachNew.description || eachNew.content}</p>
+                            </div>
+                        </div>
+                        <a class="btn-news" href="${eachNew.url}" target="_blank">Show more!</a>
                     </div>
-                    <div class="description-news">
-                        <p>${eachNew.description || eachNew.content}</p>
-                    </div>
-                </div>
-                <a class="btn-news" href="${eachNew.url}" target="_blank">Show more!</a>
-            </div>
-       
-    
+               
+            
+        
+        
+             `
+                });
+                renderNews.innerHTML += template.join('');
+        
+            }
+            const getAnything = async () => {
 
-
-     `
-        });
-        renderNews.innerHTML += template.join('');
-
+            }
+            getNews();
+            getAnything();
+        
+        }
+        catch (error) {
+            console.error(error);
+        }
+        
     }
-
-    getNews();
-
-}
-catch (error) {
-    console.error(error);
 }
