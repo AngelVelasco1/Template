0de8ed56865080117da86080a1de2d7fee519e8e
AngelVelasco1 <angelgg2020@outlook.com>
2023-05-02 09:17:08 -0500

    feat: Add footer

--- components/api.js
+++ components/api.js
@@ -2,20 +2,18 @@ export default {
     async allAPI() {
         try {
             /* DOM selections */
-           
+            
             /* Variables */
             const country = 'us';
-        
             /* API settings */
             const apiKey = `084e0cc135174cb4bad811a6571ae62a`
             let urlNews = await fetch(`https://newsapi.org/v2/top-headlines?pageSize=100&country=${country}&apiKey=${apiKey}`);
             const responseJson = await urlNews.json();
         
-            /* templates */
+            /* Templates */
             const getNews = async () => {
                 const template = responseJson.articles.map((eachNew) => {
                     return `
-                
                     <div class="news-card">
                         <div class="container-img">
                             <img class="img_news" src="${eachNew.urlToImage || " ./img/newspaper.png"}" />
@@ -30,21 +28,16 @@ export default {
                         </div>
                         <a class="btn-news" href="${eachNew.url}" target="_blank">Show more!</a>
                     </div>
-               
-            
-        
-        
              `
                 });
                 renderNews.innerHTML += template.join('');
-        
+      
             }
             const getAnything = async () => {
 
             }
             getNews();
             getAnything();
-        
         }
         catch (error) {
             console.error(error);
